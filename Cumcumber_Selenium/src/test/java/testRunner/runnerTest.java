94d5b9a11efb538205cab9a3d5397d2efa81840c
Andy Hu <andytjhu@gmail.com>
2019-05-27 20:27:45 +1000

    add missing test runner

---
+++ Cumcumber_Selenium/src/test/java/testRunner/runnerTest.java
@@ -0,0 +1,17 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+features = "src/test/java/Features",
+glue= {"testSteps"},
+plugin = {"pretty", "json:c:/target/report.json"}, 
+monochrome = true
+)
+
+public class runnerTest {
+}
